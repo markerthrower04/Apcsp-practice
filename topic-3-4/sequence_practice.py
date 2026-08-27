791a28dc9bd7bcef13e7494576e1390a50a59fef
markerthrower04 <kayvon.sadat29@student.tamdistrict.org>
2026-08-27 18:36:57 +0000

    3.4 practice

---
+++ topic-3-4/sequence_practice.py
@@ -0,0 +1,20 @@
+scores = [72, 86, 91, 68, 88]
+title = "weekly score report"
+
+#1
+print(str(scores[0]) + "," + str(scores[2]) + "," + str(scores[4]))
+
+#3
+scores.append(93)
+print(scores)
+
+#4
+weekly = title[0:6]
+report = title[13:]
+print (weekly)
+print (report)
+
+#5
+label = weekly + ":" + str(scores[0])
+print(label)
+
