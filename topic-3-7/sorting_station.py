b9edeb5b2cdb07a1a34e072ae46f9828ccd2fe47
markerthrower04 <kayvon.sadat29@student.tamdistrict.org>
2026-09-23 05:30:06 +0000

    sorting station updated

--- topic-3-7/sorting_station.py
+++ topic-3-7/sorting_station.py
@@ -1,37 +1,35 @@
 
 Label = input()
 
-shape = Label[0:4]
-color = Label[4:7]
+shape = Label[0:4].upper()
+color = Label[4:7].upper()
 size = int(Label[7:10])
 mass = int(Label[10:14])
-condition = Label[14]
+condition = Label[14].upper()
 
-destination = "E"
-doinwit = "Box"
 
 if condition == "D" or size > 50 or mass > 2000:
    destination = "INSPECT"
     
-elif shape == "BALL" and color == "Red" and size > 10:
+elif shape == "BALL" and color == "RED" and size > 10:
     destination = "B"
 elif shape ==  "BALL":
     destination = "A"
 
-if shape == "cube" and (color == "BlU" or color == "GRN") and size <= 10:
+elif shape == "CUBE" and (color == "BLU" or color == "GRN") and size <= 10:
     destination = "C"
-elif shape == "cube":
+elif shape == "CUBE":
     destination = "D"
+else:
+    destination = "E"
 
-if destination == "INSPECT":
-    doinwit = "HOLD"
-elif shape == "CONE" or mass > 1000:
-    doinwit = "CRATE"
-elif shape == "BALL":
-    doinwit = "PADDED"
-
-
+    
 
 print(destination)
-print(doinwit)
+
+
+
+
+
+
 
