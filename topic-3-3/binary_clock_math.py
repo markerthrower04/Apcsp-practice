738c290f6a3fbcd9b5b762026f19f8662b6fdd4b
markerthrower04 <kayvon.sadat29@student.tamdistrict.org>
2026-09-25 04:16:47 +0000

    updated Binary clock math

--- topic-3-3/binary_clock_math.py
+++ topic-3-3/binary_clock_math.py
@@ -20,7 +20,7 @@
 
 seconds = 59
 next_seconds = (seconds + 1) % 60
-print(next_seconds)
+
 
 clock_value = [13, 42]
 labels = ["hours", "minutes"]
@@ -28,7 +28,7 @@ labels = ["hours", "minutes"]
 clock_value.append(17)
 labels.append("seconds")
 
-selected_index = 2
+selected_index = 0
 
 clock_value = clock_value[selected_index]
 label = labels[selected_index]
@@ -49,7 +49,13 @@ bit_32 = remaining % 2
 remaining = remaining // 2
 
 clock_bit = ((bit_32), (bit_16), (bit_8), (bit_4), (bit_2), (bit_1))
-print(*clock_bit[0:])
+print("clock_bit: " + str(clock_bit[0:]))
 
-check_value = clock_value
-print(label + ":" + str(check_value))
+bit_text = str(bit_32) + str(bit_16) + str(bit_8) + str(bit_4) + str(bit_2) + str(bit_1)
+print("bit_text: " + bit_text)
+
+check_value = int(bit_text,2)
+print("check_value: " + str(check_value))
+
+print(label + ": " + str(clock_value) + " -> " + str(bit_text))
+
